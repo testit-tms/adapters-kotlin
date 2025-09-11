b76e2327a81be2197cc0c145614e8135d438ef81
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2025-09-11 19:24:22 +0300

    feat: cloud update 11.09

--- testit-kotlin-commons/build.gradle.kts
+++ testit-kotlin-commons/build.gradle.kts
@@ -1,5 +1,5 @@
 group = "ru.testit"
-version = "0.6.5"
+version = "0.6.6"
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -14,7 +14,7 @@ java {
 }
 
 val slf4jVersion = "1.7.2"
-val apiVersion = "0.7.4"
+val apiVersion = "0.7.5"
 dependencies {
     implementation("ru.testit:testit-api-client-kotlin:$apiVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
